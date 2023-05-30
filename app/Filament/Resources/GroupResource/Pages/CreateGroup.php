50cc3aedb482ab4abae5c31e32617e5603a14dd2
Gush <kakirmsplatoon2@gmail.com>
2023-05-30 11:43:30 +0100

    admin user resource

--- app/Filament/Resources/GroupResource/Pages/CreateGroup.php
+++ app/Filament/Resources/GroupResource/Pages/CreateGroup.php
@@ -10,6 +10,8 @@ class CreateGroup extends CreateRecord
 {
     protected static string $resource = GroupResource::class;
 
+    protected static bool $canCreateAnother = false;
+
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,15 +3,20 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Resources\UserResource\Pages\EditUser;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Pages\Page;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
 
 class UserResource extends Resource
 {
@@ -26,6 +31,26 @@ class UserResource extends Resource
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(50),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->maxLength(255)
+                    ->dehydrateStateUsing(static fn (null|string $state): null|string => filled($state) ? Hash::make($state) : null,)->required(static fn(Page $livewire) : bool => $livewire instanceof CreateUser,)
+                    ->dehydrated(static fn(null|string $state): bool => filled($state),)
+                    ->label(static fn(Page $livewire): string => ($livewire instanceof EditUser) ? 'New Password' : 'Password',),
+                Forms\Components\Select::make('group_id')
+                    ->relationship('group', 'name'),
+            
+                
+            // add space here 
+            
+                // Forms\Components\Toggle::make('is_owner')
+                //     ->label('Owner')
+                //     ->required(),
+                
             ]);
     }
 
@@ -35,6 +60,8 @@ class UserResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
+                Tables\Columns\TextColumn::make('Group.name')
+                    ->placeholder(''),
             ])
             ->filters([
                 //
